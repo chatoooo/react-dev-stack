a38a88bc9eba96ab10970b58ce0ef633c5719847
Jan Schmied <schmied@acsc.cz>
2015-07-19 22:10:27 +0200

    Added clean and static tasks to gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,13 +2,22 @@
 
 import gulp from 'gulp';
 import gutil from 'gulp-util';
+import grename from 'gulp-rename';
+import gclean from 'gulp-clean';
 import webpack from 'webpack';
 import webpack_config from './config/webpack.js'; 
 import WebpackDevServer from 'webpack-dev-server';
 
-gulp.task('default', ['webpack']);
 
-gulp.task("webpack", (callback) => {
+gulp.task('default', ['serve']);
+
+gulp.task('static', () => {
+  gulp.src('src/pages/template.html')
+    .pipe(grename('index.html'))
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task("webpack", ["static"], (callback) => {
     // run webpack
     webpack(webpack_config , (err, stats) => {
         if(err) throw new gutil.PluginError("webpack", err);
@@ -19,7 +28,12 @@ gulp.task("webpack", (callback) => {
     });
 });
 
-gulp.task("serve", ['webpack'], (callback) => {
+gulp.task("clean",(cb) => {
+  return gulp.src('build', {read: false})
+    .pipe(gclean({force: true}));
+});
+
+gulp.task("serve", ['webpack'], () => {
     var compiler = webpack(webpack_config)
     var server = new WebpackDevServer(compiler,{
   // webpack-dev-server options
@@ -36,18 +50,11 @@ gulp.task("serve", ['webpack'], (callback) => {
     aggregateTimeout: 300,
     poll: 1000
   },
+
   publicPath: "/assets/",
   headers: { "X-Custom-Header": "yes" },
   stats: { colors: true },
-
-  // Set this as true if you want to access dev server from arbitrary url.
-  // This is handy if you are using a html5 router.
   historyApiFallback: false,
-
-  // Set this if you want webpack-dev-server to delegate a single path to an arbitrary server.
-  // Use "*" to proxy all paths to the specified server.
-  // This is useful if you want to get rid of 'http://localhost:8080/' in script[src],
-  // and has many other use cases (see https://github.com/webpack/webpack-dev-server/pull/127 ).
   proxy: {
     "/v1/*": "http://localhost:8080"
   }
